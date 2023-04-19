a6b4eecb5d84dcd6991e14705f863e77ed49c03b
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2023-04-19 19:31:41 +0330

    Grasp Started

---
+++ mainGRASP.cpp
@@ -0,0 +1,19 @@
+#include "Interface.h"
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+int main(){
+    File file("pr01");
+    file.read_file();
+    float totalProfit = 0;
+    totalProfit = calculate_profit();
+    cout << "Total profit is --> " << totalProfit << '\n';
+    TOP top(file.get_N(), file.get_V());
+    vector<int> firstSolution = top.grasp_solution_generator();
+    cout << "***************** First Solution *****************" << '\n';
+    for (int i = 0; i < firstSolution.size(); i++) {
+        cout << firstSolution[i] << ' ';
+    }
+    cout << '\n';
+}
